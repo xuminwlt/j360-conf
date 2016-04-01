4c1cf7227fda9a05092f545995fb7b583819f47a
min_xu <xumin_wlt@163.com>
2016-04-01 15:59:08 +0800

    none

--- j360-conf-service/src/main/java/me/j360/conf/service/Bootstrap.java
+++ j360-conf-service/src/main/java/me/j360/conf/service/Bootstrap.java
@@ -1,10 +1,27 @@
 package me.j360.conf.service;
 
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.concurrent.CountDownLatch;
+
 public class Bootstrap {
 
     public static void main(String[] args) throws InterruptedException {
         System.out.print(args);
+
+        System.setProperty("resources.config.path","/Users/min_xu/Documents/IdeaProjects/fotoplace/apps/app2.properties");
+
+        final ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("classpath:conf-service-context.xml");
+        ctx.start();
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            @Override
+            public void run() {
+                ctx.close();
+            }
+        });
+        CountDownLatch countDownLatch = new CountDownLatch(1);
+        countDownLatch.await();
     }
 
 }
