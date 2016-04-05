b3da5388033863a6d1777654c194cecb08b404cb
min_xu <xumin_wlt@163.com>
2016-04-05 22:48:12 +0800

    jsw bootstrap args

--- j360-conf-jsw/src/main/java/me/j360/conf/jsw/Bootstrap.java
+++ j360-conf-jsw/src/main/java/me/j360/conf/jsw/Bootstrap.java
@@ -1,9 +1,17 @@
 package me.j360.conf.jsw;
 
+import org.apache.commons.lang3.ArrayUtils;
+
 public class Bootstrap {
 
     public static void main(String[] args) throws InterruptedException {
-        System.out.print(args);
+        if(ArrayUtils.isNotEmpty(args)){
+            // 打印参数
+            for (String arg : args)
+                System.out.println("j360-jsw-> " + arg);
+        }else{
+            System.out.println("j360-jsw args = null");
+        }
     }
 
 }
